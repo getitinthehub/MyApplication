1b102f61cfe9a29b3a7493d3601e7c2820257977
CEO of this Hub <ncniels@gmail.com>
2017-05-09 13:03:25 +0200

    update9520171303

---
+++ app/src/main/java/com/example/niels/myapplication/User.java
@@ -0,0 +1,10 @@
+package com.example.niels.myapplication;
+
+/**
+ * Created by Niels on 9-5-2017.
+ */
+
+public class User {
+    private int id;
+    private String name, screenName, description;
+}
