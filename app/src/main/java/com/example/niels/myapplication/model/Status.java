47f4801974f3d23cc588284fee597fc8c3683218
Thomas Holleman <thomasholleman@gmail.com>
2017-05-16 12:51:37 +0200

    added toJson method

--- app/src/main/java/com/example/niels/myapplication/model/Status.java
+++ app/src/main/java/com/example/niels/myapplication/model/Status.java
@@ -1,5 +1,8 @@
 package com.example.niels.myapplication.model;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Date;
 
 /**
@@ -13,10 +16,33 @@ import java.util.Date;
  * @version 1.0
  */
 
-public class Status {
+public class Status
+{
     private String text, inReplyToStatusId, inReplyToUserId;
     private int retweetCount, favoriteCount;
     private boolean retweeted, favorited;
     private Date date;
     private User user;
+    
+    public String toJson()
+    {
+        JSONObject jsonObject = new JSONObject();
+        try
+        {
+            jsonObject.put("text",text);
+            jsonObject.put("inReplyToStatusId", inReplyToStatusId);
+            jsonObject.put("inReplyToUserId", inReplyToUserId);
+            jsonObject.put("retweetCount", retweetCount);
+            jsonObject.put("favoriteCount", favoriteCount);
+            jsonObject.put("retweeted", retweeted);
+            jsonObject.put("favorited", favorited);
+            jsonObject.put("date", date);
+            jsonObject.put("user", user);
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+        return jsonObject.toString();
+    }
 }
--- app/src/main/java/com/example/niels/myapplication/model/User.java
+++ app/src/main/java/com/example/niels/myapplication/model/User.java
@@ -7,7 +7,7 @@ package com.example.niels.myapplication.model;
 
 public class User {
     private String id, name, screenName, location, description, backgroundColor, backgroundImageUrl, profileImageUrl, profileBannerUrl, language;
-    private Boolean isProtected, Following, FollowRequestSend, isQuoteStatus;
+    private Boolean isProtected, following, followRequestSend, isQuoteStatus;
     private int followersCount, friendsCount, listedCount, statusesCount, retweetCount, favoriteCount;
 
     public String getId() {
