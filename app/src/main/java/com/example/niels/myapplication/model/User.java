855e5edfbd76149f179214e580f64d40f7edf7a8
Neluz <ncniels@ymail.com>
2017-05-16 12:27:00 +0200

    Jsonutil class + user class updated

--- app/src/main/java/com/example/niels/myapplication/model/User.java
+++ app/src/main/java/com/example/niels/myapplication/model/User.java
@@ -9,4 +9,16 @@ public class User {
     private String id, name, screenName, location, description, backgroundColor, backgroundImageUrl, profileImageUrl, profileBannerUrl, language;
     private Boolean isProtected, Following, FollowRequestSend, isQuoteStatus;
     private int followersCount, friendsCount, listedCount, statusesCount, retweetCount, favoriteCount;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getScreenName() {
+        return screenName;
+    }
 }
